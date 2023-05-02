4b776173dc52085f76095f2ee236fa80a70077d8
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-02 10:25:02 +0900

    :sparkles: Feat: 각 페이지별 1차 라우팅완료 (이후에 수정이 필요할수도 있음)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Notfound from "./pages/NotFound/NotFound";
+import PayMent from "./pages/Payment/Payment";
+import SellerCenter from "./pages/SellerCenter/SellerCenter";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import ProductUpload from "./pages/ProductUpload/ProductUpload";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
+import Signup from "./pages/Signup/Signup";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="productdetail/:id" element={<ProductDetails />} />
+
+        <Route path="/cart" element={<ShoppingCart />} />
+
+        <Route path="/payment" element={<PayMent />} />
+
+        <Route path="/seller" element={<SellerCenter />} />
+
+        <Route path="/upload" element={<ProductUpload />} />
+
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
