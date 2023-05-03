14a7353c4d95c94de66cb0d75bf35490d40d8d5e
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-03 11:24:06 +0900

    :label: MarkUp: Home에 Navbar 컴포넌트추가 (기능없이 마크업만)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
-  return <div>홈입니다.</div>;
+  return (
+    <div>
+      <NavBar />
+      본문내용...
+    </div>
+  );
 };
 
 export default Home;
