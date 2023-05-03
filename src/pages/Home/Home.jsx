c1bb26aed8ced2c095bee1126add5d8fb8ec5586
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-03 11:53:41 +0900

    :label: MarkUp: 캐러셀 컴포넌트 추가 (확인용)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import Carousel from "../../components/Carousel/Carousel";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      본문내용...
+      <Carousel />
     </div>
   );
 };
