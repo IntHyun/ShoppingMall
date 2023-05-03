b22ad4c6597d75a930d5b7220b0dbe79edb01b6e
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-03 11:21:55 +0900

    :lipstick: Design : reset css 적용

--- src/App.js
+++ src/App.js
@@ -8,17 +8,48 @@ import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import ProductUpload from "./pages/ProductUpload/ProductUpload";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import Signup from "./pages/Signup/Signup";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset}
+  
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    input, textarea { 
+      -moz-user-select: auto;
+      -webkit-user-select: auto;
+      -ms-user-select: auto;
+      user-select: auto;
+    }
+    input:focus {
+      outline: none;
+    }
+
+    button {
+      border: none;
+      background: none;
+      padding: 0;
+      cursor: pointer;
+    }
+`;
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        <Route path="productdetail/:id" element={<ProductDetails />} />
+        <Route path="/productdetail/:id" element={<ProductDetails />} />
 
         <Route path="/cart" element={<ShoppingCart />} />
 
