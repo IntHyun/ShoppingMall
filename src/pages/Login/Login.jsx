5c551ba94ced094eec26e127c12a0a80573d5c0f
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-12 21:58:48 +0900

    :label: MarkUp: 로그인 페이지 부분완성

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import Logo from "../../components/Logo/Logo";
+
+import { LogoImgLink } from "../../components/NavBar/NavBarStyle";
+
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLoginHeader = styled.header`
+  margin-top: 100px;
+  margin-bottom: 70px;
+`;
+
+const StyledLoginForm = styled.form`
+  width: 550px;
+  height: 352px;
+  margin: 0 auto;
+  border: 1px solid gray;
+  border-radius: 10px;
+`;
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <StyledLoginHeader>
+        <LogoImgLink to="/">
+          <Logo />
+        </LogoImgLink>
+      </StyledLoginHeader>
+
+      <StyledLoginForm>
+        <button>구매회원 로그인</button>
+        <button>판매회원 로그인</button>
+
+        <label htmlFor="id">아이디</label>
+        <input id="id" type="text" />
+
+        <label htmlFor="pw">비밀번호</label>
+        <input id="pw" type="text" />
+      </StyledLoginForm>
+    </div>
+  );
 };
 
 export default Login;
