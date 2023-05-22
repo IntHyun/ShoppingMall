e98e0429b142a60cfaa0510e5c8bb3f7d96ef1e2
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-05-22 23:11:43 +0900

    :sparkles: Feat: 회원가입 페이지 판매회원가입, 구매회원가입 버튼 구현

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useState } from "react";
+
+import {
+  StyledLoginWrapper,
+  StyledLoginBtnWrapper,
+  StyledBuyerLoginBtn,
+  StyledSellerLoginBtn,
+} from "../Login/LoginStyle";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [buyerClicked, setBuyerClicked] = useState(true);
+  const [buyerOrSeller, setBuyerOrSeller] = useState("BUYER");
+  const [sellerClicked, setSellerClicked] = useState(false);
+
+  const handleBuyerClick = () => {
+    setBuyerClicked(true);
+    setSellerClicked(false);
+    setBuyerOrSeller("BUYER");
+  };
+
+  const handleSellerClick = () => {
+    setBuyerClicked(false);
+    setSellerClicked(true);
+    setBuyerOrSeller("SELLER");
+  };
+
+  return (
+    <div>
+      <StyledLoginWrapper>
+        <StyledLoginBtnWrapper>
+          <StyledBuyerLoginBtn clicked={buyerClicked} onClick={handleBuyerClick}>
+            구매회원가입
+          </StyledBuyerLoginBtn>
+          <StyledSellerLoginBtn clicked={sellerClicked} onClick={handleSellerClick}>
+            판매회원가입
+          </StyledSellerLoginBtn>
+        </StyledLoginBtnWrapper>
+      </StyledLoginWrapper>
+    </div>
+  );
 };
 
 export default Signup;
